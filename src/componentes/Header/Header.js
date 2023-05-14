4f7a08867e2b64e41d1e498e0a8196b9b5bfaddb
lwamae <49068653+lwamae@users.noreply.github.com>
2023-05-13 20:45:05 -0500

    styled components

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -1,8 +1,11 @@
+import { styled } from "styled-components"
 import "./Header.css"
+import Boton from "../Boton"
 
 function Header() {
     return <header className="header">
-        <img src="/img/header.png" alt='Org' />
+        <img src="/img/header.png" alt='logo' />
+        <Boton></Boton>
     </header>
 }
 
